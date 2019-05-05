dfdde1586e444e76c2875b68547eb92119d489ca
tmoux <tmou@imsa.edu>
2019-05-05 17:12:42 -0500

    5/5/19

--- codeforces/1100s/1156/f/F.cpp
+++ codeforces/1100s/1156/f/F.cpp
@@ -4,7 +4,7 @@ using ll = long long;
 
 const int M = 998244353;
 const int maxn = 5005;
-int n, a[maxn];
+int n, m;
 
 ll modexp(ll x, ll n) {
     if (n == 0) return 1;
@@ -21,21 +21,51 @@ void madd(int& a, int b) {
     a = (a+b) % M;
 }
 
+int mult(int a, int b) {
+    return (1LL*a*b) % M;
+}
 
-int dp[maxn][maxn]; //dp[i][j], i = current, j = total # cards
+int dp[maxn][maxn]; //dp[i][j], i = prev selected, j = total # cards
+map<int,int> mp;
+vector<int> a;
+int cnt[maxn];
+int inverse[maxn];
 
 int main()
 {
-    ios_base::sync_with_stdio(false); cin.tie(NULL);
+    //ios_base::sync_with_stdio(false); cin.tie(NULL);
     cin >> n;
-    map<int,int> mp;
+    //precompute inverses
+    for (int i = 0; i <= n; i++) {
+        inverse[i] = modInverse(i);
+    }
+    //read input
     for (int i = 1; i <= n; i++) {
         int ai; cin >> ai;
-        
+        if (!mp.count(ai)) a.push_back(ai);
+        ++mp[ai];
     }
-    sort(a+1,a+n+1);
-
-
+    a.push_back(0);
+    cnt[0]++;
+    sort(a.begin(),a.end());
+    for (auto p: mp) {
+        cnt[p.first] = p.second;
+    }
+    m = a.size();
+    for (int j = 1; j <= n; j++) {
+        //cout << j << endl;
+        for (int i = 0; i < m; i++) {
+            dp[j-1][i] = mult(dp[j-1][i],mult(cnt[a[i]],inverse[j]));
+            if (i > 0) madd(dp[j-1][i],dp[j-1][i-1]);
+        }
+        for (int i = 0; i < m; i++) {
+            int ai = a[i];
+            dp[j][i] = mult(cnt[ai]-1,inverse[j]);
+            madd(dp[j][i],(dp[j-1][m-1]-dp[j-1][i]+M)%M);
+        }
+    }
+    int ans = dp[n][0];
+    cout << ans << '\n';
     return 0;
 }
 
---
+++ codeforces/1100s/1156/f/gen.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+echo 5000
+for ((a=1; a <= 5000 ; a++))
+do
+    echo $a
+done
+
