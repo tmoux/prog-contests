defe8d7729e9f5510bc68bc98fb351f537030f0a
Timothy Mou <timothymou7@gmail.com>
2023-10-10 15:29:10 -0500

    Upsolve FHC E

--- misc/FHC/2023/Round1/E/sol.cpp
+++ misc/FHC/2023/Round1/E/sol.cpp
@@ -52,15 +52,191 @@ ostream& operator<<(ostream &os, const T_container &v) {
 mt19937 rng(chrono::steady_clock::now().time_since_epoch().count());
 // }}}
 
+struct DSU {
+  int n;
+  vector<int> par;
+  DSU(int _n) {
+    n = _n;
+    par.resize(n+1, -1);
+  }
+
+  int Find(int i) {
+    return par[i] < 0 ? i : par[i] = Find(par[i]);
+  }
+
+  bool Union(int x, int y) { // return true if x and y were not connected
+    x = Find(x);
+    y = Find(y);
+    if (x == y) return false;
+    if (par[x] > par[y]) swap(x, y);
+    par[x] += par[y];
+    par[y] = x;
+    return true;
+  }
+};
+
+namespace Trie {
+  const int ALPHA = 26;
+  struct Node {
+    Node *ch[ALPHA];
+    int idx;
+    Node() {}
+    Node(int _idx) : idx(_idx) {
+      F0R(i, ALPHA) ch[i] = nullptr;
+    }
+  };
+
+  const int MX = 3e6+6;
+  Node nodes[MX];
+  int cur = 0;
+  Node* reset() {
+    cur = 0;
+    nodes[cur] = Node(cur);
+    return &nodes[cur];
+  }
+
+  Node* newNode() {
+    ++cur;
+    nodes[cur] = Node(cur);
+    return &nodes[cur];
+  }
+
+  int insert(Node *root, const string& s) {
+    for (char c: s) {
+      int i = c - 'a';
+      if (!root->ch[i]) root->ch[i] = newNode();
+      root = root->ch[i];
+    }
+    return root->idx;
+  }
+
+  void dfs(Node *root, int d, vector<vector<int>>& depth) {
+    depth[d].push_back(root->idx);
+    F0R(i, ALPHA) {
+      if (root->ch[i]) {
+        dfs(root->ch[i], d+1, depth);
+      }
+    }
+  }
+};
+
+const int block_size = 750;
+struct Query {
+  int l, r, idx;
+  bool operator<(Query other) const
+  {
+    return make_pair(l / block_size, r) <
+      make_pair(other.l / block_size, other.r);
+  }
+};
+
 ll solve() {
   int N; cin >> N;
+  vector<string> S(N);
+  Trie::Node* root = Trie::reset();
+  vector<int> node(N);
+  int maxdepth = 0;
+  F0R(i, N) {
+    cin >> S[i];
+    reverse(all(S[i]));
+    ckmax(maxdepth, sz(S[i]));
+    node[i] = Trie::insert(root, S[i]);
+  }
+  vector<vector<int>> depth(maxdepth+1);
+  Trie::dfs(root, 0, depth);
 
+  int Q; cin >> Q;
+  vector<vector<Query>> Qs(maxdepth+1);
+  F0R(i, Q) {
+    int a, b, k; cin >> a >> b >> k;
+    a--, b--;
+
+    if (k <= maxdepth) Qs[k].push_back({a, b, i});
+  }
+
+  DSU dsu(Trie::cur+1);
+
+  const int MX = 1100;
+  vector<int> cnt(Trie::cur+1), freq(MX+1);
+
+  ll C = 0;
+
+  vector<int> answers(Q);
+  auto MO = [&](vector<Query> queries, int k) -> void {
+    sort(queries.begin(), queries.end());
+
+    // INIT
+    auto add = [&](int idx) -> void {
+      C++;
+      if (sz(S[idx]) < k) return;
+      int i = dsu.Find(node[idx]);
+      freq[min(cnt[i], MX)]--;
+      cnt[i]++;
+      freq[min(cnt[i], MX)]++;
+    };
+
+    auto remove = [&](int idx) -> void {
+      C++;
+      if (sz(S[idx]) < k) return;
+      int i = dsu.Find(node[idx]);
+      freq[min(cnt[i], MX)]--;
+      cnt[i]--;
+      freq[min(cnt[i], MX)]++;
+    };
+
+    auto get_answer = [&]() -> int {
+      int ans = 0;
+      for (int i = 1; i <= MX; i++) {
+        ans += min(freq[i], i - ans);
+      }
+      return ans;
+    };
+
+    int cur_l = 0;
+    int cur_r = -1;
+    // invariant: data structure will always reflect the range [cur_l, cur_r]
+    for (Query q : queries) {
+      while (cur_l > q.l) {
+        cur_l--;
+        add(cur_l);
+      }
+      while (cur_r < q.r) {
+        cur_r++;
+        add(cur_r);
+      }
+      while (cur_l < q.l) {
+        remove(cur_l);
+        cur_l++;
+      }
+      while (cur_r > q.r) {
+        remove(cur_r);
+        cur_r--;
+      }
+      answers[q.idx] = get_answer();
+    }
+
+    // reset
+    for (int i = cur_l; i <= cur_r; i++) remove(i);
+  };
+
+  for (int i = maxdepth; i >= 1; i--) {
+    for (auto node: depth[i]) {
+      F0R(j, Trie::ALPHA) {
+        Trie::Node* n = &Trie::nodes[node];
+        if (n->ch[j]) dsu.Union(node, n->ch[j]->idx);
+      }
+    }
+    MO(Qs[i], i);
+  }
+  DEBUG(C);
+  return std::accumulate(all(answers), 0LL);
 }
 
 int main() {
   ios_base::sync_with_stdio(false); cin.tie(NULL);
   int T; cin >> T;
   FOR(tc, 1, T+1) {
+    cerr << "On case " << tc << endl;
     cout << "Case #" << tc << ": " << solve() << '\n';
   }
 }
